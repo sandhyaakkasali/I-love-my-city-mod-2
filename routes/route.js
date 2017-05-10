e2170a2c9a62d866102d515d8059925fb9383e3b
sandhyaakkasali <sandhya.akkasali@mindtree.com>
2017-05-10 14:54:29 +0530

    code changes
    code changes

--- routes/route.js
+++ routes/route.js
@@ -1,6 +1,6 @@
 exports.home=function(req,res){
   res.render('index',{
-    sitename:'I Love My City',
+    sitename:'I Love My City ',
     title:'I Love My City',
     headline:'Baskaran - Module 2',
   });
@@ -12,10 +12,9 @@ exports.city=function(req,res){
     var imageCount=4;
     
 
-    if(cityName === 'chennai'){
-       title="Chennai";
-       heading ="Chennai, formerly known as Madras, is the capital of the state of Tamil Nadu and is India's fourth largest city. It is located on the Coromandel Coast of the Bay of Bengal. With an estimated population of 8.9 million (2014), the 400-year-old city is the 31st largest metropolitan area in the world. The city of Madras in 1909 Chennai boasts of a long history from the English East India Company, through the British Raj to its evolution in the late 20th century as a services and manufacturing hub for India. Additionally, the pre-city area of Chennai has a long history within the records of South Indian Empires.";
-    }
+    if(cityName === 'kolkata'){
+       title="kolkata";
+       heading ="kolkata, the capital of the Indian state of West Bengal. Located on the east bank of the Hooghly River, it is the principal commercial, cultural, and educational centre of East India, while the Port of Kolkata is India's oldest operating port and its sole major riverine port. }
     else if(cityName === 'mysore'){
        title ="Mysuru";
        heading="The Kingdom of Mysore was a kingdom in southern India, traditionally believed to have been founded in 1399 in the vicinity of the modern city of Mysore. The kingdom, which was ruled by the Wodeyar family, initially served as a vassal state of the Vijayanagara Empire. With the decline of the Vijayanagara Empire (c. 1565), the kingdom became independent. The 17th century saw a steady expansion of its territory and during the rule of Narasaraja Wodeyar I and Chikka Devaraja Wodeyar, the kingdom annexed large expanses of what is now southern Karnataka and parts of Tamil Nadu to become a powerful state in the southern Deccan.";
